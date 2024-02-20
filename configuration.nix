59d8cc60c8beac9aa364505b01545167eecb1c6b
rw <rw@jjk.is>
2024-02-19 18:20:11 -0800

    add tailscale

--- configuration.nix
+++ configuration.nix
@@ -3,14 +3,32 @@
 {
   system.stateVersion = "23.05"; 
   nix.settings.experimental-features = [ "nix-command" "flakes" ];
+  nixpkgs.config.allowUnfree = true;
 
+  users.users.alice = {
+    isNormalUser = true;
+    extraGroups = [ "wheel" ]; # Enable ‘sudo’ for the user.
+    packages = with pkgs; [ ];
+    # set shell to zsh 
+    # passwordFile = "/persist/passwords/alice";
+  };
+
+  # allow alice to run sudo without password
+  security.sudo.wheelNeedsPassword = false;
+
+
+  services.tailscale = {
+    enable = true;
+    authKeyFile = "/tsauthkey";
+    extraUpFlags = ["--ssh" "--hostname" "basic"];
+  };
 
   # Caddy service configuration.
   services.caddy = {
     enable = true;
     extraConfig = ''
       0.0.0.0
-      
+
       respond "Hello, world!"
     '';
   };
