aed117b6e1e33b0bd7f150dd5a0de3960112792a
rw <rw@jjk.is>
2024-02-19 23:02:21 -0800

    more mvp

--- configuration.nix
+++ configuration.nix
@@ -6,23 +6,6 @@ let
 in
 {
   system.stateVersion = "23.05";
-  nix.settings.experimental-features = [ "nix-command" "flakes" ];
-  nixpkgs.config.allowUnfree = true;
-
-  users.users.alice = {
-    isNormalUser = true;
-    extraGroups = [ "wheel" ]; # Enable ‘sudo’ for the user.
-    packages = with pkgs; [ ];
- };
-
-  # allow alice to run sudo without password
-  security.sudo.wheelNeedsPassword = false;
-
-  services.tailscale = {
-    enable = true;
-    authKeyFile = "/tsauthkey";
-    extraUpFlags = [ "--ssh" "--hostname" "basic" ];
-  };
 
   services.caddy = {
     enable = true;
