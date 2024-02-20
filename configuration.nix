598b14a85f6ccc19ba908c2bdc5a0a555940f09e
rw <rw@jjk.is>
2024-02-19 17:41:40 -0800

    even more mvp

--- configuration.nix
+++ configuration.nix
@@ -4,16 +4,10 @@
   system.stateVersion = "23.05"; 
   nix.settings.experimental-features = [ "nix-command" "flakes" ];
 
-  users.users.caddy = {
-    isNormalUser = true;
-    extraGroups = [ "wheel" ]; # Enable ‘sudo’ for the user.
-    packages = with pkgs; [ ];
-  };
 
   # Caddy service configuration.
   services.caddy = {
     enable = true;
-    user = "caddy";
     # Configure Caddy to serve a simple "Hello, world!" page.
     config = ''
       :80 {
